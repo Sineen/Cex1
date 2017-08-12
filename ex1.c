a9965ba0dd4018f77b535492ac00c7232db8558b
Sineen <sineen.nawatha@mail.huji.ac.il>
2017-08-12 17:23:05 +0300

    reaf file added ( needs to check pointers adn call the functions with the righ array pointers

--- ex1.c
+++ ex1.c
@@ -5,19 +5,25 @@
 #include "ex1.h"
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
+#include <stdlib.h>
+#definde TRUE = 1;
+#definde FALSE = 0;
+
 const double ALFA = 0.012299; // Moon mass / earth mass
 const double BETA = (0.012299 - 1);// we always use it the the function in -B form so I multiplied it here by -1 an dI will use it as +B in the functions when needed
 //todo check how to generalize alfa and beta
 
-double timelabs;// T/n = dt
-double numberOfTimesToCheck; //n
-double prints;// m
+long double *timelabs;// T/n = dt
+long double *numberOfTimesToCheck; //n
+long double *prints;// m
 
 double distance(long double locationX, long double locationY, double parameter);
 double acceleration(long double locationX, long double locationY, long double velocity, int flag );
 void newLocation(long double *location, long double *velocity);
 void newVelocity(long double *velocity, long double acceleration);
 void forwardEuler(long double *locationX, long double *locationY,long double *velocityX, long double *velocityY, double timeSteps);
+void readfile();
 
 double distance(long double locationX, long double locationY, double parameter)
 /**
@@ -43,6 +49,8 @@ double acceleration(long double locationX, long double locationY, long double ve
 {
     double distance1 = distance(locationX,locationY, ALFA);
     double distance2 = distance(locationX,locationY, BETA);
+    assert(distance1!=0);
+    assert(distance2!=0);
     if (isXAxis){
         return ( locationX + (2*velocity)+ (BETA *((locationX + ALFA )/distance1)) - (ALFA *((locationX+BETA)/distance2)));
     }
@@ -93,9 +101,7 @@ void forwardEuler(long double *locationX, long double *locationY,long double *ve
     {
         return 0;
     }
-    else
-    {
-        // get the new location since its all in pointers teh orginal var changes value
+        // get the new location since its all in pointers the orginal var changes value
         newLocation(*locationX,*velocityX);
         newLocation(*locationY,*velocityY);
         //calculate acceleration
@@ -108,28 +114,114 @@ void forwardEuler(long double *locationX, long double *locationY,long double *ve
             writesInOutputfile(*locationX,*locationY);
         }
         forwardEuler(*locationX,*locationY,*velocityX,*velocityY,timeSteps-1);
-    }
+
 
 }
 
 void writesInOutputfile(long double locationX, long double locationY)
+/**
+ * this function would take the outpu and add it o the outputfile and print it to the screen ?
+ * @param locationX  the new location in the x axis
+ * @param locationY  the new location in the y axis
+ */
 {
 
 }
 
-int main(int argc, char **argv)
+void readfile(long double ** arr, File* inFile)
 {
-    File* inputFile = (argv[1],"r");
-    File* outputFile = (argv[2],"w");
-    if (inputFile==NULL)
+    int index =0 ;
+    int decimalPoint=FALSE;
+    int afterDecimalPoint=1;
+    int returnVal;
+    while(!feof(inFile))// didnt get to end of file keep reading chars
     {
-        fprintf(stderr, "Error no input file");
-        printf("Error no input file"); //todo how to send to stderr???
-        exit(-1);
+
+        char c = fgetc(inFile);
+        if(c =="<"){
+            c = fgetc(inFile);
+        }
+        if(c==" "){
+            c = fgetc(inFile);
+        }
+        if(c==".")
+        {
+            decimalPoint=TRUE;
+            continue;
+        }
+        if ( decimalPoint)
+        {
+            afterDecimalPoint= afterDecimalPoint*10;
+        }
+        if( c == "," || c==">" ) // going to the new aray var
+        {
+            if (decimalPoint && afterDecimalPoint == 1) // deals with input like < 999. , 8, 9 , 9 >
+            {
+                returnVal = -1;
+                fprintf(stdeff, "bad format input");
+            }
+            {
+                if (decimalPoint) {
+                    decimalPoint = FALSE;
+                    arr[index] = array[index] / afterDecimalPoint;
+                }
+                index++;
+                continue;
+            }
+        }
+            if( c == "\n")
+            {
+                if (index < 4) // example <4 ,4 , 4>\n <4 ,4 ,4>
+                {
+                    returnVal=-1;
+                    fprintf(stderr,"Missing variables ");
+                    continue;
+                }
+                else
+                {
+                    if(index > 4) // example <4, 4, 4, 4, 4>\n <4,4,4>
+                    {
+                        returnVal=-1;
+                        fprintf(stderr,"Extra variables ");
+                    }
+                    continue;
+                }
+            }
+            else
+            {
+                *(arr[index])= *(arr[index]) * 10  + c;
+            }
+        }
+        return (returnVal);
+
+
+
     }
+    int main(int argc, char **argv)
+    {
+        File* inputFile = (argv[1],"r");
+        File* outputFile = (argv[2],"w");
+        if (inputFile==NULL)
+        {
+            fprintf(stderr, "Error no input file");
+            printf("Error no input file"); //todo how to send to stderr???
+            exit(-1);
+        } else
+        {
+            long double **arVariables[7]={0,0,0,0,0,0,0}; // array with (0 location x, 1 location y , 2 velocityx, 3 velocity Y, 4 Time , 5 n , 6 m )
+            int badInput = readfile(**arVariables,inputFile);
+            if (badInput){
+                fprint(stderr, "bad input file ");
+                exit(-1);
+            }
+            //get the file input to variables then send them to funcuion
+        }
+
+        fclose(inputFile);
+        fclose(outputFile);
 
 
 
 
-    return 0;
-}
+        return 0;
+    }
